41f49d7554bd1e2859860e618348e0dbf24f6779
mamun-mahmood <mamamun1999@gmail.com>
2023-08-29 17:06:11 +0600

    dashboard layout added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,24 @@
 import "./Dashboard.css";
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div className="dashboard-container">
+      <div className="first-row">
+        <div className="column"></div>
+        <div className="column"></div>
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
+      <div className="second-row">
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
+      <div className="third-row">
+        <div className="column"></div>
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
