1710e50b892af237dfda0d10dbc1c2a5548637e2
mamun-mahmood <mamamun1999@gmail.com>
2023-08-29 18:15:16 +0600

    added coins card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 import { svg } from "../assets/svg";
 import "./Dashboard.css";
 const Dashboard = () => {
-  const ballance = {
-    estimated: {
-      total: 123978,
-      monthlyProfit: 2560.78,
-      growth: 14.67,
-    },
-    btcusdt: {
+  const estimatedBalance = {
+    name: "Estimated Balance",
+    total: 123978,
+    monthlyProfit: 2560.78,
+    growth: 14.67,
+  };
+  const coins = [
+    {
+      name: "Bitcoin",
+      symbol: "BTCUSDT",
       total: 23738,
       pnlDaily: -16.78,
       growth: 14.67,
@@ -53,8 +56,11 @@ const Dashboard = () => {
           y: 30,
         },
       ],
+      image: "/images/btc.png",
     },
-    ethusdt: {
+    {
+      name: "Ethereum",
+      symbol: "ETHUSDT",
       total: 23738,
       pnlDaily: 189.78,
       growth: 14.67,
@@ -100,8 +106,11 @@ const Dashboard = () => {
           y: 30,
         },
       ],
+      image: "/images/eth.png",
     },
-    solusdt: {
+    {
+      name: "Solana",
+      symbol: "SOLUSDT",
       total: 23738,
       pnlDaily: 556.78,
       growth: 14.67,
@@ -147,35 +156,57 @@ const Dashboard = () => {
           y: 30,
         },
       ],
+      image: "/images/sol.png",
     },
-  };
-  const { estimated, btcusdt, ethusdt, solusdt } = ballance;
+  ];
+  const { growth, monthlyProfit, name, total } = estimatedBalance;
   return (
     <div className="dashboard-container">
       <div className="first-row">
-        <div className="column estimated-balance-card">
+        <div className="column card estimated-balance-card">
           <div className="card-header">
             <img src="/images/wallet.png" alt="wallet-icon" />
-            <h3>Estimated Balance</h3>
+            <h3>{name}</h3>
             <img src="/images/eye.png" alt="eye-icon" />
           </div>
           <div className="card-body">
-            <h3 className="balance">${estimated.total}</h3>
+            <h3 className="balance">${total}</h3>
             <div className="profit">
               <p className="profit-text">Monthly Profit</p>
               <p className="profit-value">
-                {estimated.monthlyProfit < 1 ? "-" : "+"}$
-                {estimated.monthlyProfit}
+                {monthlyProfit < 1 ? "-" : "+"}${monthlyProfit}
               </p>
               <p className="growth">
-                {estimated.growth < 1 ? "-" : "+"}${estimated.growth}
+                {growth < 1 ? "-" : "+"}${growth}
               </p>
             </div>
           </div>
         </div>
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
+        {coins?.map(
+          ({ growth, name, total, graphData, pnlDaily, image, symbol }) => (
+            <div className="column card coin-card">
+              <div className="card-header">
+                <img src={image} alt="wallet-icon" />
+                <div className="name-symbol">
+                  <h3>{symbol}</h3>
+                  <p>{name}</p>
+                </div>
+              </div>
+              <div className="card-body">
+                <h3 className="balance">${total}</h3>
+                <div className="profit">
+                  <p className="profit-text">PNL Daily</p>
+                  <p className="profit-value">
+                    {pnlDaily < 1 ? "-" : "+"}${pnlDaily}
+                  </p>
+                  <p className="growth">
+                    {growth < 1 ? "-" : "+"}${growth}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
+        )}
       </div>
       <div className="second-row">
         <div className="column"></div>
